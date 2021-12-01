39683870187ec3c85026092b43295e19ed0850f1
Paserno <paserno.x@gmail.com>
2021-11-30 21:46:28 -0300

    Primer Commit - Gif-App

---
+++ src/GifExpertApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const GifExpertApp = () => {
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr/>
+        </>
+    );
+}
+
+
+export default GifExpertApp;
